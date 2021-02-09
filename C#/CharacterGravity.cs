08bd57ae58504a94e012f9996af75505611ecae6
Jean-LouisH <LouisLou512.e@gmail.com>
2021-02-08 22:15:29 -0500

    Added character controller based prototype scripts

---
+++ C#/CharacterGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGravity : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 characterVelocity = Vector3.zero;
+    private float gravity = -9.81f;
+
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    public Vector3 CalculateVelocity()
+    {
+        if (characterController.isGrounded && characterVelocity.y < 0.0f)
+        {
+            characterVelocity.y = 0.0f;
+        }
+
+        characterVelocity.y += gravity * Time.deltaTime;
+        return characterVelocity;
+    }
+}
---
+++ C#/CharacterMovementRelativeToCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterMovementRelativeToCamera : MonoBehaviour
+{
+    public float movementSpeed_m_per_s;
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        cameraTransform = gameObject.transform.Find("Camera");
+    }
+
+    public Vector3 CalculateVelocity()
+    {
+        float horizontalAxis = Input.GetAxisRaw("LEFT_AXIS_X");
+        float verticalAxis = Input.GetAxisRaw("LEFT_AXIS_Y");
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+        cameraForward.y = 0.0f;
+        cameraRight.y = 0.0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+        return ((cameraForward * verticalAxis) + (cameraRight * horizontalAxis)) * movementSpeed_m_per_s;
+    }
+}
